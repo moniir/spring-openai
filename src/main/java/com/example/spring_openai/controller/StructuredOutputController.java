aae0ad79b8d32f2441f742ecdfe5ba161eed0cce
Monir Hossain <monir198@gmail.com>
2026-04-05 19:44:02 +0600

    Added AI text to java object

---
+++ src/main/java/com/example/spring_openai/controller/StructuredOutputController.java
@@ -0,0 +1,33 @@
+package com.example.spring_openai.controller;
+
+import com.example.spring_openai.model.CountryCities;
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+
+@RestController
+@RequestMapping("/api")
+public class StructuredOutputController {
+
+    private final ChatClient chatClient;
+
+    public StructuredOutputController(ChatClient.Builder chatClientBuilder) {
+        this.chatClient = chatClientBuilder
+                .defaultAdvisors(new SimpleLoggerAdvisor())
+                .build();
+    }
+
+    @GetMapping("/chat-bean")
+    public ResponseEntity<CountryCities> chatBean(@RequestParam("question") String question) {
+        CountryCities countryCities = chatClient.prompt()
+                .user(question)
+                .call().entity(CountryCities.class);
+        return ResponseEntity.ok(countryCities);
+    }
+
+}
---
+++ src/main/java/com/example/spring_openai/model/CountryCities.java
@@ -0,0 +1,6 @@
+package com.example.spring_openai.model;
+
+import java.util.List;
+
+public record CountryCities(String country, List<String> cities) {
+}
