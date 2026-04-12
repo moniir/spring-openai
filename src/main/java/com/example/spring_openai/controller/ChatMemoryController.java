624c9fb211d95eaeb227bbac1a7840db3d590b30
Monir Hossain <monir198@gmail.com>
2026-04-12 22:25:04 +0600

    Added user wise chat memory

--- src/main/java/com/example/spring_openai/controller/ChatMemoryController.java
+++ src/main/java/com/example/spring_openai/controller/ChatMemoryController.java
@@ -4,9 +4,12 @@ import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import static org.springframework.ai.chat.memory.ChatMemory.CONVERSATION_ID;
 
 @RestController
 @RequestMapping("/api")
@@ -21,6 +24,14 @@ public class ChatMemoryController {
     @GetMapping("/chat-memory")
     public ResponseEntity<String> chatMemory(@RequestParam("message") String message) {
         return ResponseEntity.ok(chatClient.prompt().user(message)
+                .call().content());
+    }
+
+    @GetMapping("/user-chat-memory")
+    public ResponseEntity<String> chatMemory(@RequestHeader("username") String username,
+                                             @RequestParam("message") String message) {
+        return ResponseEntity.ok(chatClient.prompt().user(message)
+                        .advisors(advisorSpec -> advisorSpec.param(CONVERSATION_ID, username))
                 .call().content());
     }
 }
